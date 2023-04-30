d913670f09399a95e56cb872f02e2c4498970296
ab <ab@my-mac-0.local>
2023-05-01 01:26:41 +0900

    change variable type

--- queue/slice.go
+++ queue/slice.go
@@ -8,34 +8,38 @@ var ErrBufferOverflow = errors.New("buffer overflow")
 // Its buffer size doesn't grow.
 type BufferedQueue[T any] struct {
 	buf  []T
-	head int
-	tail int
+	head uint
+	tail uint
+}
+
+func (q *BufferedQueue[T]) len() uint {
+	return uint(len(q.buf))
 }
 
 // Enqueue enqueues a value to queue.
 // If buffer is full before enqueuing, ErrBufferOverflow is returned.
 func (q *BufferedQueue[T]) Enqueue(value T) error {
-	if (q.tail+2)%len(q.buf) == q.head {
+	if (q.tail+2)%q.len() == q.head {
 		return ErrBufferOverflow
 	}
-	q.tail = (q.tail + 1) % len(q.buf)
+	q.tail = (q.tail + 1) % q.len()
 	q.buf[q.tail] = value
 	return nil
 }
 
 // Dequeue returns (value, true) if any value is existing in queue, (default-value, false) if queue is empty.
 func (q *BufferedQueue[T]) Dequeue() (T, bool) {
-	if (q.tail+1)%len(q.buf) == q.head {
+	if (q.tail+1)%q.len() == q.head {
 		var v T
 		return v, false
 	}
 	v := q.buf[q.head]
-	q.head = (q.head + 1) % len(q.buf)
+	q.head = (q.head + 1) % q.len()
 	return v, true
 }
 
 // NewBufferedQueue returns empty BufferedQueue which has specified size.
-func NewBufferedQueue[T any](size int) *BufferedQueue[T] {
+func NewBufferedQueue[T any](size uint) *BufferedQueue[T] {
 	return &BufferedQueue[T]{
 		buf:  make([]T, size+1),
 		head: 0,
