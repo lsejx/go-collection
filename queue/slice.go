82ff3e0adbf5a723ce3bd68183c8e46e1c46cc59
ab <ab@my-mac-0.local>
2023-05-01 09:14:35 +0900

    add method

--- queue/slice.go
+++ queue/slice.go
@@ -12,29 +12,49 @@ type BufferedQueue[T any] struct {
 	tail uint
 }
 
-func (q *BufferedQueue[T]) len() uint {
+func (q *BufferedQueue[T]) cap() uint {
 	return uint(len(q.buf))
+}
+
+// Cap returns capacity of queue.
+func (q *BufferedQueue[T]) Cap() uint {
+	return q.cap() - 1
+}
+
+// Len returns number of items which queue is holding.
+func (q *BufferedQueue[T]) Len() uint {
+	if (q.tail+1)%q.cap() == q.head {
+		return 0
+	}
+	if q.head > q.tail {
+		return q.tail + q.cap() - q.head + 1
+	}
+	return q.tail - q.head + 1
+}
+
+func (q *BufferedQueue[T]) IsFull() bool {
+	return q.Len() == q.Cap()
 }
 
 // Enqueue enqueues a value to queue.
 // If buffer is full before enqueuing, ErrBufferOverflow is returned.
 func (q *BufferedQueue[T]) Enqueue(value T) error {
-	if (q.tail+2)%q.len() == q.head {
+	if (q.tail+2)%q.cap() == q.head {
 		return ErrBufferOverflow
 	}
-	q.tail = (q.tail + 1) % q.len()
+	q.tail = (q.tail + 1) % q.cap()
 	q.buf[q.tail] = value
 	return nil
 }
 
 // Dequeue returns (value, true) if any value is existing in queue, (default-value, false) if queue is empty.
 func (q *BufferedQueue[T]) Dequeue() (T, bool) {
-	if (q.tail+1)%q.len() == q.head {
+	if (q.tail+1)%q.cap() == q.head {
 		var v T
 		return v, false
 	}
 	v := q.buf[q.head]
-	q.head = (q.head + 1) % q.len()
+	q.head = (q.head + 1) % q.cap()
 	return v, true
 }
 
